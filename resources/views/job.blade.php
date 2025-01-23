79e4858c9bbb99d9396086566f446574e411d4bb
Slavcho Mitrov <mitrov.slavcho@gmail.com>
2025-01-23 15:52:12 +0100

    forms

--- resources/views/job.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layout>
-    <x-slot:heading>Job</x-slot>
-    <h2 class="text-lg font-bold">{{ $job["title"] }}</h2>
-    <p>This job pays {{ $job["salary"] }} per year.</p>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,66 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => Job::with('employer')->cursorPaginate(3)
+    return view('jobs.index', [
+        'jobs' => Job::with('employer')->latest()->simplePaginate(3)
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
+Route::get('/jobs/create', function() {
+    return view('jobs.create');
+});
 
-    return view('job', [
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::findOrFail($id);
+
+    return view('jobs.show', [
         'job' => $job
     ]);
+});
+
+Route::post('/jobs', function() {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    Job::create([
+       'title' => request('title'),
+       'salary' => request('salary'),
+       'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
+});
+
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', [
+        'job' => $job
+    ]);
+});
+
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+Route::delete('/jobs/{id}', function ($id) {
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
