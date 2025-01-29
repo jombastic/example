958bf81b43c03fea91e1fdf02a75f00346973793
Slavcho Mitrov <mitrov.slavcho@gmail.com>
2025-01-29 12:29:46 +0100

    login and registration

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,19 +1,16 @@
-@props(["active" => false, "type" => "a"])
+@props(['active' => false, 'type' => 'a'])
 
-@if ($type === "a")
-    <a
-        class="{{ $active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium"
-        aria-current="{{ $active ? "page" : "false" }}"
-        {{ $attributes }}
-    >
+@php
+    $classes = 'rounded-md px-3 py-2 text-sm font-medium ';
+    $classes .= $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white';
+@endphp
+
+@if ($type === 'a')
+    <a aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes->merge(['class' => $classes]) }}>
         {{ $slot }}
     </a>
 @else
-    <button
-        class="{{ $active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium"
-        aria-current="{{ $active ? "page" : "false" }}"
-        {{ $attributes }}
-    >
+    <button aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes->merge(['class' => $classes]) }}>
         {{ $slot }}
     </button>
 @endif
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,10 @@ Route::view('/contact', 'contact');
 
 Route::resource('jobs', Controllers\JobController::class);
 
+// Auth
+Route::get('/register', [Controllers\RegisteredUserController::class, 'create']);
+Route::post('/register', [Controllers\RegisteredUserController::class, 'store']);
+
+Route::get('/login', [Controllers\SessionController::class, 'create']);
+Route::post('/login', [Controllers\SessionController::class, 'store']);
+Route::post('/logout', [Controllers\SessionController::class, 'destroy']);
